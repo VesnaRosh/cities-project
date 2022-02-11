7c7cbaf04b6ebe392c5843000ccd721e9e3e5e9c
VesnaRosh <80645534+VesnaRosh@users.noreply.github.com>
2022-02-11 12:37:30 +0100

    Update polution.js

--- cities - Copy/pages/api/polution.js
+++ cities - Copy/pages/api/polution.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   let city = req.body.city;
-  await fetch(`https://api.waqi.info/feed/${city}/?token=1901f5c95020ad8ac45e5226f8d10b19c8279ff7`, {
+  await fetch(`https://api.waqi.info/feed/${city}/?token=`, {
     method: 'GET'
   }).then(response => response.json())
     .then(data => { res.status(200).json({ aqi: data.data.aqi }) })
